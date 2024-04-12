5f140cc2e1e667883fb70421a15c297db37d30f8
anchalpatel <aanchalpatel4404@gmail.com>
2024-04-12 20:32:51 +0530

    file upoad function implementation

--- server/index.js
+++ server/index.js
@@ -5,16 +5,22 @@ const app = express();
 const database = require("./config/database");
 const {cloudinaryConnect} = require("./config/cloudinary");
 const cookieParser = require('cookie-parser');
-
+const fileUpload = require("express-fileupload");
 
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 database.dbConnection();
 cloudinaryConnect();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+    fileUpload({
+        useTempFiles : true,
+        tempFileDir : '/tmp/'
+    })
+)
 
 app.use(cors({
     origin: "*",
