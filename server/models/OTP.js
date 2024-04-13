2490ded55a4821f946a0e170b49f2e550e3789a2
himanshi1810 <98298127+himanshi1810@users.noreply.github.com>
2024-04-13 08:42:21 +0530

    EmailTemplate added in otp madel

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const mailSender = require("../utils/mailSender");
+const emailTemplate = require("../templates/emailVerification");
 const OTPSchema = new mongoose.Schema({
 	email: {
 		type: String,
@@ -27,6 +28,7 @@ async function sendVerificationEmail(email, otp) {
 		const mailResponse = await mailSender(
 			email,
 			"Verification Email",
+			emailTemplate(otp)
 		);
 		console.log("Email sent successfully: ", mailResponse);
 	} catch (error) {
