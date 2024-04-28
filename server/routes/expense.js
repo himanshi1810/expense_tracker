be79257735761264a9d993c86dcd5034af1221f0
himanshi1810 <98298127+himanshi1810@users.noreply.github.com>
2024-04-28 10:16:08 +0530

    Additional Function Done

--- server/routes/expense.js
+++ server/routes/expense.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const { auth } = require("../middlewares/auth");
-const { addExpense, editExpense, viewExpense, deleteExpense } = require("../controllers/Expense");
+const { addExpense, editExpense, viewExpense, deleteExpense, viewGroupDailyExpenses, viewGroupMonthlyExpenses, groupTotalExpense } = require("../controllers/Expense");
 const router = express.Router();
 
 router.post("/addExpense", auth, addExpense);
 router.post("/updateExpense", auth, editExpense);
 router.get("/viewExpense", viewExpense);
 router.delete("/deleteExpense", auth, deleteExpense);
+router.get("/viewGroupDailyExpenses", auth, viewGroupDailyExpenses);
+router.get("/viewGroupMonthlyExpenses", auth, viewGroupMonthlyExpenses);
+router.get("/viewGroupDailyExpenses", auth, viewGroupDailyExpenses);
+router.get("/groupTotalExpense", auth, groupTotalExpense);
 module.exports = router;
