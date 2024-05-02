fbf1598aaf2a50e469185f70d501a8860fe23154
anchalpatel <aanchalpatel4404@gmail.com>
2024-05-03 01:19:29 +0530

    Reducer added

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import {Provider} from 'react-redux'
 import "./index.css";
+import {Toaster} from "react-toastify"
+import {BrowserRouter} from "react-router-dom"
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./Reducer";
+const store = configureStore({
+  reducer : rootReducer
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}></Provider>
+      <BrowserRouter>
+        <Toaster/>
+        <App></App>
+      </BrowserRouter>
   </React.StrictMode>
 );
