31f5bf47df89eac35e2e5c7bcaf032fec8ba05eb
himanshi1810 <98298127+himanshi1810@users.noreply.github.com>
2024-05-04 14:28:05 +0530

    final authentication pages done

--- client/src/Components/Core/Auth/Template.js
+++ client/src/Components/Core/Auth/Template.js
@@ -11,26 +11,26 @@ function Template({ img, title, title2, description1, formType }) {
       {loading ? (
         <div className="spinner"></div>
       ) : (
-        <div className="mx-auto flex w-11/12 max-w-maxContent flex-col-reverse justify-evenly gap-y-12 py-12 md:flex-row md:gap-y-0 md:gap-x-12">
-          <div className="relative mx-auto w-11/12 max-w-[450px] md:mx-0">
+        <div className="mx-auto flex w-11/12 max-w-maxContent flex-col-reverse justify-evenly gap-y-12 py-12 md:flex-row md:gap-y-0 md:gap-x-12 ">
+          <div className="relative mx-auto w-11/12 max-w-[450px] md:mx-0 ">
             <img
-              src={img} // Make sure to include the img prop here
+              src={img} 
               alt="logo"
               width={700}
               height={700}
               loading="lazy"
-              className="absolute -top-4 right-4 z-10"
+              className=""
             />
           </div>
           <div className="mx-auto w-11/12 max-w-[450px] md:mx-0 ml-12">
-            <h1 className="text-[1.875rem] font-semibold leading-[2.375rem] text-white">
+            <h1 className="text-[1.875rem] font-semibold leading-[2.375rem] text-white-100 ">
               {title}
-              <span className="text-[1.875rem] font-semibold leading-[2.375rem] text-primary">
+              <span className="text-[1.875rem] font-semibold leading-[2.375rem] text-blue-400">
                 {title2}
               </span>
             </h1>
             <p className="mt-4 text-[1.125rem] leading-[1.625rem]">
-              <span className="text-grey-100">{description1}</span>{" "}
+              <span className="text-gray-400">{description1}</span>{" "}
             </p>
             {formType === "signup" ? <SignupForm /> : <LoginForm />}
           </div>
--- client/src/Pages/ForgotPassword.js
+++ client/src/Pages/ForgotPassword.js
@@ -22,21 +22,21 @@ function ForgotPassword() {
         <div className="spinner"></div>
       ) : (
         <div className="mx-auto flex w-11/12 max-w-maxContent flex-col-reverse justify-evenly gap-y-12 py-12 md:flex-row md:gap-y-0 md:gap-x-12">
-        <div className="relative mx-auto w-11/12 max-w-[450px] md:mx-0">
+        <div className="relative mx-auto w-11/12 max-w-[450px] md:mx-0 ">
             <img
               src={img} 
               alt="logo"
               width={700}
               height={700}
               loading="lazy"
-              className="absolute -top-4 right-4 z-10"
+              className=""
             />
           </div>
           <div className="max-w-[500px] p-4 lg:p-8">
             <h1 className="text-[1.875rem] font-semibold leading-[2.375rem] text-white">
               {!emailSent ? "Reset your password" : "Check email"}
             </h1>
-            <p className="my-4 text-[1.125rem] leading-[1.625rem] text-grey-100">
+            <p className="my-4 text-[1.125rem] leading-[1.625rem] text-gray-400">
               {!emailSent
                 ? "Have no fear. We'll email you instructions to reset your password. If you dont have access to your email we can try account recovery"
                 : `We have sent the reset email to ${email}`}
@@ -44,8 +44,8 @@ function ForgotPassword() {
             <form onSubmit={handleOnSubmit}>
               {!emailSent && (
                 <label className="w-full">
-                  <p className="mb-1 text-[1rem] leading-[1.375rem] text-white">
-                    Email Address <sup className="text-white">*</sup>
+                  <p className="mb-1 text-[1rem] leading-[1.375rem] text-white-100">
+                    Email Address <sup className="text-white-100">*</sup>
                   </p>
                   <input
                     required
@@ -54,13 +54,13 @@ function ForgotPassword() {
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     placeholder="Enter email address"
-                    className="form-style w-full bg-grey-100 text-white py-2 rounded-lg px-2"
+                    className="form-style w-full bg-gray-400 text-white-100 py-2 rounded-lg px-2"
                   />
                 </label>
               )}
               <button
                 type="submit"
-                className="mt-6 w-full rounded-[8px] bg-primary py-[12px] px-[12px] font-bold text-white"
+                className="mt-6 w-full rounded-[8px] bg-blue-400 py-[12px] px-[12px] font-bold text-white-100"
               >
                 {!emailSent ? "Sumbit" : "Resend Email"}
               </button>
